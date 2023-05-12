40fd828e635a31df9d0711c85cda0aaad95ab60c
Cupelt <57065968+Cupelt@users.noreply.github.com>
2023-05-12 21:40:20 +0900

    initialization project

---
+++ src/main/java/com/cupelt/castapi/CastAPI.java
@@ -0,0 +1,17 @@
+package com.cupelt.castapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import java.util.logging.Logger;
+
+public final class CastAPI extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
